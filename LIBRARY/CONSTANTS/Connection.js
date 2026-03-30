feb9e4b1d19a3e30dde02967223cf10f36347d11
f3be20b4-2c39-4c6a-b6f5-76ded0d8e655 <erandix9@example.com>
2026-03-30 17:39:39 +0300

    Updates

--- LIBRARY/CONSTANTS/Connection.js
+++ LIBRARY/CONSTANTS/Connection.js
@@ -1,15 +1,31 @@
 import { BODY } from "./DataBase/Body/Body.js";
+import { CREATEDATABASELINK } from "./DataBase/CreateDataBaseLink/CreateDataBaseLink.js";
+import { CREATETABLELINK } from "./DataBase/CreateTableLink/CreateTableLink.js";
+import { GETDATABASELINK } from "./DataBase/GetDataLink/GetDataLink.js";
+import { GETEMAILLINK } from "./DataBase/GetEmailLink/getEmailLink.js";
+import { INSERTDATALINK } from "./DataBase/InsertDataLink/InsertDataLink.js";
+import { PAYMENTLINK } from "./DataBase/PaymentLink/PaymentLink.js";
+import { SENDEMAILLINK } from "./DataBase/SendEmailLink/SendEmailLink.js";
+import { UPDATEDATALINK } from "./DataBase/UpdateDataLink/UpdateDataLink.js";
 
 export const CONSTANTSCONNECTIONS=()=>{
 
-    const DATA=`const BODY=${BODY};
-    `;
+const DATA=`const BODY=${BODY};
+const CREATEDATABASELINK="${CREATEDATABASELINK}";
+const CREATETABLELINK="${CREATETABLELINK}";
+const GETDATABASELINK="${GETDATABASELINK}";
+const GETEMAILLINK="${GETEMAILLINK}";
+const INSERTDATALINK="${INSERTDATALINK}";
+const PAYMENTLINK="${PAYMENTLINK}";
+const SENDEMAILLINK="${SENDEMAILLINK}";
+const UPDATEDATALINK="${UPDATEDATALINK}";
+`;
 
-    localStorage.setItem("CONSTANTS",DATA);
+localStorage.setItem("CONSTANTS",DATA);
 
 };
 
 `
-const BODY="${BODY}";
+const UPDATEDATALINK="${UPDATEDATALINK}";
 
 `
